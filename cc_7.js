ea795e246b1b68d90e361517d713cce6c2cef387
Amanda Robles <amandarobles@usf.edu>
2025-02-11 11:05:46 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -8,3 +8,14 @@ function calculateInvoice(subtotal, taxRate, discount) {
 //test data
 calculateInvoice(100, 0.08, 5); //Expected output: "Total Invoice: $103.00"
 calculateInvoice(500, 0.1, 20); //Expected output: "Total Invoice: $530.00"
+
+// task 2: Function Expression
+//Declaring function expression that calculates hourly wage
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    let hourlyWage = salary / (hoursPerWeek * 52);
+    console.log(`Hourly Wage: $${hourlyWage.toFixed(2)}`);
+};
+
+//test data
+calculateHourlyWage(52000, 40); //Expected output: "Hourly Wage: $25.00"
+calculateHourlyWage(75000, 35); //Expected output: "Hourly Wage: $41.21"
