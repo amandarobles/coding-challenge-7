2ea37fc3fd3579d3f14259948f9b260ca4d4df81
Amanda Robles <amandarobles@usf.edu>
2025-02-11 10:52:40 -0500

    Task 1- Customer Invoice Calculation

---
+++ cc_7.js
@@ -0,0 +1,10 @@
+// task 1: Function Declaration
+//Calcuating final invoice amount
+function calculateInvoice(subtotal, taxRate, discount) {
+    let total = (subtotal + (subtotal * taxRate)) - discount;
+    console.log(`Total Invoice: $${total.toFixed(2)}`);
+}
+
+//test data
+calculateInvoice(100, 0.08, 5); //Expected output: "Total Invoice: $103.00"
+calculateInvoice(500, 0.1, 20); //Expected output: "Total Invoice: $530.00"
